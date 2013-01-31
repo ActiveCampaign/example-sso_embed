9b3e8cec12f2fdac6a8a881f703bd563c755d18e
Matt Thommes <matt@activecampaign.com>
2013-01-31 09:48:07 -0600

    initial commit: index file added, and updated readme

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
-example-sso_embed
-=================
+ActiveCampaign Custom API Script: Use SSO to link users to ActiveCampaign (bypassing the login), or embed as an iFrame.
 
-Use SSO to link users to ActiveCampaign (bypassing the login), or embed as an iFrame.
+## Requirements
+
+1. [Our PHP API library](https://github.com/ActiveCampaign/activecampaign-api-php)
+2. A web server where you can run PHP code
+
+## Installation and Usage
+
+You can install **example-sso_embed** by downloading (or cloning) the source.
+
+Input your ActiveCampaign URL and API Key. Example below:
+
+<pre>
+$api_url = "https://ACCOUNT.api-us1.com";
+$api_key = "4f3c6d12f0.....00ca273778dc893";
+</pre>
+
+Add values for the SSO variables at the top:
+
+<pre>
+$sso_ip_address = "127.0.0.1";
+$sso_username = "user1";
+$sso_duration = "30";
+</pre>
+
+Make sure the path to the PHP library is correct:
+
+<pre>
+require_once("../../activecampaign-api-php/includes/ActiveCampaign.class.php");
+</pre>
+
+## Documentation and Links
+
+* [SSO API documentation](http://www.activecampaign.com/api/example.php?call=singlesignon)
+
+## Reporting Issues
+
+We'd love to help if you have questions or problems. Report issues using the [Github Issue Tracker](https://github.com/ActiveCampaign/example-sso_embed/issues) or email help@activecampaign.com.
---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+	$api_url = "https://mthommes6.api-us1.com";
+	$api_key = "4f3c6d12f03185b7ae5963916375de4201453f07f7d4ba64ea45a34b100ca273778dc893";
+	$sso_ip_address = "38.104.242.98"; // the IP address that the user will access ActiveCampaign from
+	$sso_username = "user1"; // the username of the person requesting access
+	$sso_duration = "30"; // the duration of their access token (in minutes)
+
+	define("ACTIVECAMPAIGN_URL", $api_url);
+	define("ACTIVECAMPAIGN_API_KEY", $api_key);
+
+	require_once("../../activecampaign-api-php/includes/ActiveCampaign.class.php");
+	$ac = new ActiveCampaign(ACTIVECAMPAIGN_URL, ACTIVECAMPAIGN_API_KEY);
+
+	if (!(int)$ac->credentials_test()) {
+		print_r("Invalid credentials (URL and API Key).");
+		exit();
+	}
+
+	$account = $ac->api("account/view");
+//$ac->dbg($account);
+	$account_url = "http://" . $account->account; // differs from API URL
+
+	// get the user information (should include the access token)
+	$sso_user = $ac->api("auth/singlesignon?sso_addr={$sso_ip_address}&sso_user={$sso_username}&sso_duration={$sso_duration}");
+//$ac->dbg($sso_user);
+
+	if (is_object($sso_user) && isset($sso_user->token)) {
+		$sso_token = $sso_user->token;
+
+		// use token in iframe or links
+		echo "<p><a href=\"{$account_url}/admin/main.php?_ssot={$sso_token}\">Go to ActiveCampaign</a></p>";
+
+		echo "<iframe src=\"{$account_url}/admin/main.php?_ssot={$sso_token}\" style=\"height: 500px; width: 75%;\"></iframe>";
+	}
+
+?>
